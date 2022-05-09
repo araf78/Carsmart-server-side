d01d0037e87ea4a2aa8a3dd334c59ccde38df3d9
unknown <alarafath71@gmail.com>
2022-05-09 23:06:07 +0600

    deatai info

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,32 +12,72 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try {
         await client.connect();
-        const carItem = client.db("carsmart").collection("items");
+        const carCollection = client.db("carsmart").collection("items");
+        const myItemCollection = client.db("carsmart").collection("myItems");
+
 
         // set data 
         app.get('/item', async (req, res)=>{
             const query = {};
-            const cursor = carItem.find(query);
+            const cursor = carCollection.find(query);
             const items = await cursor.toArray();
             res.send(items)
         });
+
+        // get data and  detail info 
+        app.get('/item/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const item = await carCollection.findOne(query);
+            res.send(item);
+        }) 
+
+        // add new item
+        app.post('/item',async (req,res)=>{
+            const newItem = req.body;
+            console.log('add new item',newItem);
+            const result = await carCollection.insertOne(newItem) 
+            res.send(result)
+          })
+
+        //   delevery
+        app.put('item/:id', async (req, res)=>{
+            const id = req.params.id;
+            const newQuantity = req.body; 
+            const filter = {_id: ObjectId(id)};     
+            const options = { upsert: true};
+            const deleveryDoc = {
+                $set :{
+                    quantity: newQuantity.quantity
+                }
+            };
+            const result = await carCollection.updateOne(filter, deleveryDoc, options)
+            res.send(result);
+        })
 
           // delete a user 
     app.delete('/item/:id', async (req, res)=>{
         const id = req.params.id;
         const query = {_id: ObjectId(id)};
-        const result = await carItem.deleteOne(query);
+        const result = await carCollection.deleteOne(query);
         res.send(result)
       })
 
-        // post data
-        // app.post('/user',async (req,res)=>{
-        //     const newItem = req.body;
-        //     console.log('add new user',newItem);
-        //     const result = await userCollection.insertOne(newItem) 
-        //     res.send(result)
-        //   })
+    //   post my item 
+    app.post('/myItem',async (req,res)=>{
+        const myItem = req.body;
+        const result = await myItemCollection.insertOne(myItem) 
+        res.send(result)
+      })
+      // my items 
+      app.post('/myItem', async (req, res) => {
+         const query = {};
+         const cursor = myItemCollection.find(query);
+         const myItems = await cursor.toArray();
+          res.send(myItems);
+      })
     }
+
     finally {
         // await client.close()
     }
